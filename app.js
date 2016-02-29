6b0718101918947c7b3223243a2356190dd9c720
Boyd220 <boydfranken@msn.com>
2016-02-29 18:09:57 +0100

    skelet

---
+++ app.js
@@ -0,0 +1,8 @@
+var main = function (){
+
+	"use strict";
+	window.alert('Als dit er niet op komt dan werkt het niet.'); // Deze regel mag je verwijderen , als dit er niet opkomt heb je een fout in je html document met het aanspreken van de .js file in dit geval app.js , staan de files in dezelfde map , heb je de juiste naam ... ?
+
+};
+
+$(document).ready(main);
