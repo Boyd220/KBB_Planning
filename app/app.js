3a7efb1448b573e26ae4226a527226a35f7dcce1
Boyd220 <boydfranken@msn.com>
2016-03-03 15:25:22 +0100

    Made planning pages, made controls, made layout better, animation for werknemers

--- app/app.js
+++ app/app.js
@@ -3,13 +3,37 @@ var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'ngAnimate']);
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-    when('/', {
+    when('/Werknemers', {
       title: 'Werknemers',
       templateUrl: 'partials/werknemers.html',
       controller: 'werknemersCtrl'
     })
+    .when('/Planningen', {
+      title: 'Planningen',
+      templateUrl: 'partials/planning.html',
+    })
+    .when('/Planningen/Dagplanning',{
+      title: 'Dagplanning',
+      templateUrl: 'partials/planningen/dagplanning.html',
+      controller: 'dagplanningCtrl'
+    })
+    .when('/Planningen/Weekplanning',{
+      title: 'Weekplanning',
+      templateUrl: 'partials/planningen/weekplanning.html',
+      controller: 'weekplanningCtrl'
+    })
+    .when('/Planningen/Maandplanning',{
+      title: 'Maandplanning',
+      templateUrl: 'partials/planningen/maandplanning.html',
+      controller: 'maandplanningCtrl'
+    })
+    .when('/Planningen/Jaarplanning',{
+      title: 'Jaarplanning',
+      templateUrl: 'partials/planningen/jaarplanning.html',
+      controller: 'jaarplanningCtrl'
+    })
     .otherwise({
-      redirectTo: '/'
+      redirectTo: '/Werknemers'
     });;
 }]);
     
