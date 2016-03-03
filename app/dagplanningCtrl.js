31964b60797e645695fa9d470f5980687ad5f1f5
Boyd220 <boydfranken@msn.com>
2016-03-03 19:15:36 +0100

    Fixed small things, calendar added to dayplanning

--- app/dagplanningCtrl.js
+++ app/dagplanningCtrl.js
@@ -81,3 +81,13 @@ app.controller('dagplanningenEditCtrl', function ($scope, $modalInstance, item, 
             }
         };
 });
+$(document).ready(function(){
+    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+    date_input.datepicker({
+      format: 'dd/mm/yyyy',
+      container: container,
+      todayHighlight: true,
+      autoclose: true,
+    })
+});
